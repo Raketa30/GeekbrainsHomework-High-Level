5697abfe8ebdc4826e57e61e1a392cda8c339827
duckpool <kulgachov88@gmail.com>
2021-04-11 15:36:07 +0300

    Lesson 1 done!

--- src/ru/geekbrains/lesson1/Main.java
+++ src/ru/geekbrains/lesson1/Main.java
@@ -1,4 +1,33 @@
 package ru.geekbrains.lesson1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        String[] numbers = {"one", "two", "three", "four", "five"};
+
+        System.out.println(Arrays.toString(numbers));
+        System.out.println();
+
+        swapElementsByIndex(numbers, 1, 3);
+        System.out.println(Arrays.toString(numbers));
+        System.out.println(transformToArrayList(numbers));
+    }
+
+    //1. Написать метод, который меняет два элемента массива местами.
+    // (массив может быть любого ссылочного типа);
+
+    public static <T> void swapElementsByIndex(T[] array, int i, int j) {
+        //считаю что проверки на то что индексы входят в дипазон массива излишние
+        T temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    //2. Написать метод, который преобразует массив в ArrayList;
+
+    public static <T> ArrayList<T> transformToArrayList(T[] array) {
+        return new ArrayList<>(Arrays.asList(array));
+    }
 }
---
+++ src/ru/geekbrains/lesson1/bigtask/Orange.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.lesson1.bigtask;
+
+public class Orange extends Fruit {
+    public Orange() {
+        super(1.5f, "Orange");
+    }
+}
